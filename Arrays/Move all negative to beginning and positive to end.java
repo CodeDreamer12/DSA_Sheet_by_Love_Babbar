5484977a7a0fdec875d1a0c63334faf4c9e413e1
Vanshika Jain <78136922+CodeDreamer12@users.noreply.github.com>
2023-04-04 21:23:52 +0530

    Create Move all negative to beginning and positive to end.java

---
+++ Arrays/Move all negative to beginning and positive to end.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[]={-12, 11, -13, -5, 6, -7, 5, -3, -6};
+		Arrays.sort(arr);
+		for(int i=0; i<arr.length; i++){
+		    System.out.print(arr[i]+" ");
+		}
+	}
+}
